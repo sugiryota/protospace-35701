4f0f84dbfc6dd16cafd8e34cbe6adc2457da4492
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-05-22 22:54:48 +0900

    issue2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 def create
   @comment = Comment.new(comment_params)
   if @comment.save
-    redirect_to root_path(@comment.prototype)
+    redirect_to prototype_path(@comment.prototype)
   else
     @prototype = @comment.prototype
     @comments = @prototype.comments
